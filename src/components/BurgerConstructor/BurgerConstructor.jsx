173916006c9042487835c41d6ea82e24ce3e5964
ilyavorozhtsov39 <ilyavorozhrsov39@gmail.com>
2025-01-11 20:03:47 +0300

    index page layout finished

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -1,12 +1,52 @@
 import React from "react"
 import "./BurgerConstructor.scss"
+import { ConstructorElement, DragIcon, Button, CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components"
+
+import data from '../../data.js'
 
 function BurgerConstructor() {
   return (
-    <div className="burger-constructor ml-5">
-      <p>BurgerConstructor</p>
+    <div className="burger-constructor ml-5 pt-25" >
+      <div className="burger-constructor__items pl-4 pr-4">
+        {
+          data.map((item, index) => 
+            <div className="constructor-element__container">
+              <div className="constructor-element__icon">
+                <DragIcon />
+              </div>
+              <ConstructorElement
+                text={item.name}
+                price={item.price}
+                thumbnail={item.image_mobile}
+                key={index}
+                extraClass={index !== data.length - 1 ? "mb-4" : ""}
+                type={index === 0 ? "top" : index === data.length - 1 ? "bottom" : undefined}
+              />
+            </div>
+          )
+        }
+      </div>
+      <div className="burger-constructor__result mt-10">
+        <div className="burger-constructor__result-price mr-10">
+          <p className="text text_type_digits-medium mr-2">610</p>
+          <CurrencyIcon type="primary" />
+        </div>
+        <Button 
+          type="primary"
+          size="large"
+        >
+          Оформить заказ
+        </Button>  
+      </div>
     </div>
   )
 }
 
 export default BurgerConstructor;
+
+{/* <ConstructorElement
+  text={item.name}
+  price={item.price}
+  thumbnail={item.image_mobile}
+  key={index}
+/> */}
--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -3,7 +3,6 @@ import "./BurgerIngredients.scss"
 import { Tab, CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import data from '../../data.js'
 
-const temp = data[0]
 
 const buns = data.filter(item => item.type === "bun")
 const main = data.filter(item => item.type === "main")
@@ -22,8 +21,31 @@ function BurgerIngredients() {
         <Tab value="main" active={current === "main"} onClick={setCurrent}>Начинки</Tab>
       </div>
       <div className="burger-ingredients__content">
+          <h2 className="text text_type_main-medium mt-10">Булки</h2>
           <div className="burger-ingredients__buns items">
             {buns.map((item, index) => 
+              <Ingredient 
+                id={index + 1}
+                name={item.name}
+                image={item.image}
+                price={item.price}
+                key={item.index}
+            />)}
+          </div>
+          <h2 className="text text_type_main-medium mt-10">Соусы</h2>
+          <div className="burger-ingredients__buns items">
+            {sauce.map((item, index) => 
+              <Ingredient 
+                id={index + 1}
+                name={item.name}
+                image={item.image}
+                price={item.price}
+                key={item.index}
+            />)}
+          </div>
+          <h2 className="text text_type_main-medium mt-10">Начинки</h2>
+          <div className="burger-ingredients__buns items">
+            {main.map((item, index) => 
               <Ingredient 
                 id={index + 1}
                 name={item.name}
